63fbfffd7eba1ae1c3a7f9381b234bfe9e5806f8
Scott Perkins <scottrperkins@gmail.com>
2023-05-15 19:11:33 +1200

    pre-scaffold

--- backend/Models/Manufacturer.cs
+++ backend/Models/Manufacturer.cs
@@ -4,6 +4,8 @@ public class Manufaturer{
     public int Id {get; set;}
 
     public string? ManucaturerName{get; set;}
+    
+    public string? Location {get; set;}
 
     public List<ManufacturerFood>? ManufacturerFoods {get; set;}
 }
