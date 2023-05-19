6ddfb2dcf6b99b2274918afb3a92d0e6ce8fe939
Scott Perkins <scottrperkins96@gmail.com>
2023-05-19 12:24:45 +1200

    changes and stuff

--- frontend/src/home/home.jsx
+++ frontend/src/home/home.jsx
@@ -3,16 +3,44 @@ import React, { useEffect, useState } from 'react'
 
 export const Home = () => {
 
-    const [weapons, setWeapons] = useState([])
+    // const [weapons, setWeapons] = useState([])
 
+    // useEffect(() => {
+    //     if (weapons.length === 0)
+    //         fetch(`${process.env.REACT_APP_BACKEND_URL}/Weapon`)
+    //             .then(response => response.json())
+    //             .then(data => setWeapons(data));
+    // }, [weapons]);
+
+    // console.log(weapons);
+
+    const [weapons, setWeapons] = useState([]);
+    const [error, setError] = useState(null);
+    
     useEffect(() => {
-        if (weapons.length === 0)
-            fetch(`${process.env.REACT_APP_BACKEND_URL}/Weapons`)
-                .then(response => response.json())
-                .then(data => setWeapons(data));
+        if (weapons.length === 0) {
+            fetch(`${process.env.REACT_APP_BACKEND_URL}/weapon`)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! status: ${response.status}`);
+                    }
+                    return response.json();
+                })
+                .then(data => setWeapons(data))
+                .catch(error => {
+                    console.error(error);
+                    setError(error);
+                });
+        }
     }, [weapons]);
-
+    
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+    
     console.log(weapons);
+    
+
 
     if (weapons.length === 0) return <Spinner />;
 
